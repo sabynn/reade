ea2abdf26ac079c6af6224b6bb9e6628caa6872e
sabynn <sabynamaharani@gmail.com>
2022-02-09 19:37:13 +0700

    add face analysis model

---
+++ lib/models/face_analysis.dart
@@ -0,0 +1,11 @@
+class FaceAnalysis {
+  double? smile;
+  double? rightEyeOpen;
+  double? leftEyeOpen;
+
+  FaceAnalysis({
+    this.smile,
+    this.rightEyeOpen,
+    this.leftEyeOpen,
+  });
+}
--- lib/services/controller/camera_controller.dart
+++ lib/services/controller/camera_controller.dart
@@ -3,6 +3,7 @@ import 'package:camera/camera.dart';
 class CameraManager {
   List<CameraDescription>? cameras;
   CameraController? _controller;
+  CameraDescription? cameraDescription;
 
   Future<CameraController?> load() async {
     cameras = await availableCameras();
@@ -11,8 +12,33 @@ class CameraManager {
     _controller = CameraController(
       cameras![position],
       ResolutionPreset.medium,
-      enableAudio: false,
+      enableAudio: true,
     );
+    cameraDescription = _controller?.description;
+    print("HERE WE GO AGAIN");
+    await _controller?.initialize();
+    return _controller;
+  }
+
+  Future<CameraController?> changeCamera() async {
+    print("HEHE KESINIII");
+    cameras = await availableCameras();
+    final lensDirection =  _controller?.description.lensDirection;
+    CameraDescription newDescription;
+    if(lensDirection == CameraLensDirection.front){
+      newDescription = cameras!.firstWhere((description) => description.lensDirection == CameraLensDirection.back);
+    }
+    else{
+      newDescription = cameras!.firstWhere((description) => description.lensDirection == CameraLensDirection.front);
+    }
+
+    _controller = CameraController(
+      newDescription,
+      ResolutionPreset.medium,
+      enableAudio: true,
+    );
+
+    cameraDescription = _controller?.description;
     await _controller?.initialize();
     return _controller;
   }
